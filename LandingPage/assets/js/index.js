078736b4e1cc145217523224de23be54f3567c93
vilisark <vilisark@gmail.com>
2021-03-12 22:20:34 -0300

    test

--- LandingPage/assets/js/index.js
+++ LandingPage/assets/js/index.js
@@ -1,8 +1,5 @@
 var video = document.getElementById("myVideo");
 
-// Get the button
-var btn = document.getElementById("myBtn");
-
 // Pause and play the video, and change the button text
 function myFunction() {
   if (video.paused) {
