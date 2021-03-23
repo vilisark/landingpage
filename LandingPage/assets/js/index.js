f0090b5f27589a834189552df4e465f3091e4a61
vilisark <vilisark@gmail.com>
2021-03-23 17:29:17 -0300

    simple changes

--- LandingPage/assets/js/index.js
+++ LandingPage/assets/js/index.js
@@ -1,12 +1,10 @@
-var video = document.getElementById("videoInsert");
+var video = document.getElementById("myVideo");
 
-// Pause and play the video, and change the button text
 function myFunction() {
   if (video.paused) {
     video.play();
-    btn.innerHTML = "Pause";
   } else {
     video.pause();
-    btn.innerHTML = "Play";
   }
 }
+myFunction();
